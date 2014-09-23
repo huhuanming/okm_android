951ae023c4c808f3b5df9ae87d6810c5d4813ac1
chen <924010725@qq.com>
2014-09-23 11:13:04 +0800

    框架上传

---
+++ main/src/main/java/com/okm_android/main/Fragment/HomeFragment.java
@@ -0,0 +1,17 @@
+package com.okm_android.main.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chen on 14-9-22.
+ */
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
