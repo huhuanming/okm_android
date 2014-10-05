ac280a4ad07b4057d2a0f95550af7c0218fe5dd4
chen <924010725@qq.com>
2014-10-05 17:36:41 +0800

    个人资料页面部分长传

--- main/src/main/java/com/okm_android/main/Fragment/UserFragment.java
+++ main/src/main/java/com/okm_android/main/Fragment/UserFragment.java
@@ -7,15 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.okm_android.main.R;
+
 /**
  * Created by chen on 14-9-22.
  */
 public class UserFragment extends Fragment {
+    private View parentView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().getActionBar().setDisplayShowTitleEnabled(true);
         getActivity().getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         getActivity().invalidateOptionsMenu();
-        return super.onCreateView(inflater, container, savedInstanceState);
+        parentView = inflater.inflate(R.layout.fragment_user, container, false);
+        return parentView;
     }
 }
